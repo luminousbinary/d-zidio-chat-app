1be1cfc0c69cbbbca3cd6626690f7cba701509e2
Benediction Letlhogile <benedictionletlhogile@gmail.com>
2024-07-20 16:58:37 +0200

    frontend login

---
+++ frontend/z-chat-app/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Login from "./pages/login/Login";
+
+function App() {
+  return (
+  <div className='p-4 h-screen flex items-center justify-center'>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/z-chat-app/src/pages/login/Login.jsx
@@ -0,0 +1,6 @@
+function Login() {
+  return <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+
+  </div>;
+};
+export default Login;
