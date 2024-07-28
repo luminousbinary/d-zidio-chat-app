5feb52f8a797d09c8f231a8c3da187356b1ee91a
Benediction Letlhogile <benedictionletlhogile@gmail.com>
2024-07-28 19:15:17 +0200

    updated frontend

--- frontend/z-chat-app/src/pages/signup/SignUp.jsx
+++ frontend/z-chat-app/src/pages/signup/SignUp.jsx
@@ -1,4 +1,139 @@
+import GenderCheckBox from "./GenderCheckBox";
+
 const SignUp = () => {
-  return <div>SignUp</div>
+  return (
+  <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+    <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0' >
+    <h1 class='glow'>Zidio Chat App<br />
+        <span className='text-blue-500'>Sign Up</span>
+    </h1>
+
+      <div class='container'>
+      <form>
+        <div>
+          <label className='label p-2 '>
+            <span className='text-base label-text'>Full Name</span>
+          </label>
+          <input type='text' placeholder='Benny Mac' className='w-full input input-bordered h-10' />
+        </div>
+
+        <div>
+          <label className='label p-2'>
+            <span className='text-base label-text'>Username</span>
+          </label>
+          <input 
+          type='text' 
+          placeholder='BennyMac' 
+          className='w-full input input-bordered h-10'
+          />
+        </div>
+
+      <div>
+      <label className='label'>
+            <span className='text-base label-text'>Password</span>
+          </label>
+          <input 
+          type='password' 
+          placeholder='Enter Password' 
+          className='w-full input input-bordered h-10'
+          />
+      </div>
+
+    <div>
+      <label className='label'>
+            <span className='text-base label-text'>Comfirm Password</span>
+          </label>
+          <input 
+          type='password' 
+          placeholder='Comfirm Password' 
+          className='w-full input input-bordered h-10'
+          />
+          </div>
+
+          <GenderCheckBox />
+
+          <a className='text-sm hover:underline hover:text-blue-600 mt-4 inline-block' href='#' >
+          Already have an account?
+        </a>
+
+        <div>
+        <button className='btn btn-block btn-sm mt-2'>Sign Up</button>
+      </div>
+      </form>
+      </div>
+    </div>
+  </div>
+  );
 };
 export default SignUp;
+
+
+// STARTER CODE FOR SIGNUP COMPONENT
+// import GenderCheckBox from "./GenderCheckBox";
+
+// const SignUp = () => {
+//   return (
+//   <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+//     <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0' >
+//     <h1 class='glow'>Zidio Chat App<br />
+//         <span className='text-blue-500'>Sign Up</span>
+//     </h1>
+
+//       <div class='container'>
+//       <form>
+//         <div>
+//           <label className='label p-2 '>
+//             <span className='text-base label-text'>Full Name</span>
+//           </label>
+//           <input type='text' placeholder='Benny Mac' className='w-full input input-bordered h-10' />
+//         </div>
+
+//         <div>
+//           <label className='label p-2'>
+//             <span className='text-base label-text'>Username</span>
+//           </label>
+//           <input 
+//           type='text' 
+//           placeholder='BennyMac' 
+//           className='w-full input input-bordered h-10'
+//           />
+//         </div>
+
+//       <div>
+//       <label className='label'>
+//             <span className='text-base label-text'>Password</span>
+//           </label>
+//           <input 
+//           type='password' 
+//           placeholder='Enter Password' 
+//           className='w-full input input-bordered h-10'
+//           />
+//       </div>
+
+//     <div>
+//       <label className='label'>
+//             <span className='text-base label-text'>Comfirm Password</span>
+//           </label>
+//           <input 
+//           type='password' 
+//           placeholder='Comfirm Password' 
+//           className='w-full input input-bordered h-10'
+//           />
+//           </div>
+
+//           <GenderCheckBox />
+
+//           <a className='text-sm hover:underline hover:text-blue-600 mt-4 inline-block' href='#' >
+//           Already have an account?
+//         </a>
+
+//         <div>
+//         <button className='btn btn-block btn-sm mt-2'>Sign Up</button>
+//       </div>
+//       </form>
+//       </div>
+//     </div>
+//   </div>
+//   );
+// };
+// export default SignUp;
