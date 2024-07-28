74f3c1877e396272adda89001c2e9e46668aa5fd
Benediction Letlhogile <benedictionletlhogile@gmail.com>
2024-07-28 19:16:28 +0200

    updated frontend

--- frontend/z-chat-app/src/App.jsx
+++ frontend/z-chat-app/src/App.jsx
@@ -1,8 +1,8 @@
 // import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import SignUp from "./pages/signup/SignUp";
+// import Login from "./pages/login/Login";
+// import SignUp from "./pages/signup/SignUp";
 // import { Toaster } from "react-hot-toast";
 // import { useAuthContext } from "./context/AuthContext";
 
